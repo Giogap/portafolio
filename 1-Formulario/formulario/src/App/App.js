05199bf25ab1a470f2d6e48cac86cc3c47c87f70
Giogap <giogap@gmail.com>
2024-01-11 21:01:46 -0500

    uoload form

--- 1-Formulario/formulario/src/App/App.js
+++ 1-Formulario/formulario/src/App/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import Form_B from "../Form_B/Form_B";
 import Navbar from '../Navbar/Navbar';
-import Form_A from '../Form_A/Form_A';
 
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/formb" element={<Form_B />} />
-        <Route path="/forma" element={<Form_A />} />
       </Routes>
 
     </Router>
--- 1-Formulario/formulario/src/Navbar/Navbar.js
+++ 1-Formulario/formulario/src/Navbar/Navbar.js
@@ -7,13 +7,7 @@ const Navbar = () => {
       <a href='/' className='title'>Home-Formulario</a>
       <ul>
         <li>
-          <a href='/' className='home'>Home</a>
-        </li>
-        <li>
           <a href='/formb' className='formb'>Formulario B</a>
-        </li>
-        <li>
-          <a href='/forma' className='forma'>Formulario A</a>
         </li>
       </ul>
     </nav>

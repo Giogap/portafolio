fcf9e4216023bff3b893b9e2a93ed8621aa96b35
Giogap <giogap@gmail.com>
2024-02-01 19:31:51 -0500

    upload .env

--- 1-Formulario/formulario/src/Form_B/Form_B.js
+++ 1-Formulario/formulario/src/Form_B/Form_B.js
@@ -1,33 +1,73 @@
-import React from 'react';
 import './Form_B.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function Form() {
+
+    const [nombre, setNombre] = useState("");
+    const [genero, setGenero] = useState("");
+    const [biografia, setBiografia] = useState("");
+    const [pais, setPais] = useState("");
+    const [numero_contacto, setNumero_contacto] = useState("");
+
+    const add = () => {
+        Axios.post("http://localhost:3001/guardarDatos", {
+            nombre: nombre,
+            genero: genero,
+            biografia: biografia,
+            pais: pais,
+            numero_contacto: numero_contacto,
+        }).then(() => {
+            alert("Registro Ok");
+            limpiarCampos();
+        })
+    }
+
+    const limpiarCampos = () => {
+        setNombre("");
+        setGenero("");
+        setBiografia("");        
+        setPais("");
+        setNumero_contacto("");
+      }
+
+
     return (
         <div className='container'>
             <div className='form'>
                 <div className='data'>
                     <label>Nombre
-                        <input type='text'>
+                        <input onChange={(event)=>{
+                            setNombre(event.target.value);
+                            }} type='text' value={nombre}>
                         </input>
                     </label>
                     <label>Genero
-                        <input type='text'>
+                        <input onChange={(event)=>{
+                            setGenero(event.target.value);
+                            }} type='text' value={genero}>
                         </input>
                     </label>
                     <label>Biografia
-                        <textarea>
-
+                        <textarea onChange={(event)=>{
+                            setBiografia(event.target.value);
+                            }} type='text' value={biografia}>
                         </textarea>
                     </label>
                     <label>Pais
-                        <input type='text'>
+                        <input onChange={(event)=>{
+                            setPais(event.target.value);
+                            }} type='text' value={pais}>
                         </input>
                     </label>
                     <label>Numero de Contacto
-                        <input type='text'>
+                        <input onChange={(event)=>{
+                            setNumero_contacto(event.target.value);
+                        }} type='text' value={numero_contacto}>
                         </input>
                     </label>
-                    <button type="submit">Guardar
+                    <button type="submit" onClick={add}>
+                        Guardar
                     </button>
                 </div>
             </div>
--- 1-Formulario/server/index.js
+++ 1-Formulario/server/index.js
@@ -8,6 +8,31 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "formulariop_db"
+
+});
+
+app.post("/guardarDatos", (req, res) => {
+    const nombre = req.body.nombre;
+    const genero = req.body.genero;
+    const biografia = req.body.biografia;
+    const pais = req.body.pais;
+    const numero_contacto = req.body.numero_contacto;
+
+    db.query('INSERT INTO datos(nombre, genero, biografia, pais, numero_contacto) VALUES (?, ?, ?, ?, ?)', 
+        [nombre, genero, biografia, pais, numero_contacto], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Registro Ok");
+            }
+    });
+})
+
 
 app.listen(3001, () => {
     console.log("Server Ok 3001");
